d3a85cb6ee70a6dfc91c8b28330f6fe275b03cb5
Kaoru <kaoru@gmail.com>
2024-06-20 10:30:00 +0800

    完成lottroy.js排序修改

--- lottroy_step.js
+++ lottroy_step.js
@@ -1,11 +1,7 @@
 
-<<<<<<< HEAD
-let testEl=document.querySelector("#test");
-=======
 // 抓取 <div id="test">
 let testEl=document.querySelector("#test");
 // 看內文
->>>>>>> 2751ca2 (1.index.js樂透邏輯)
 console.log(testEl.textContent);
 testEl.innerHTML=`<ol>`
 
@@ -14,10 +10,6 @@ let lottroy=[];
 
 for(let j=0; j<count;j++){
 let number=[];
-<<<<<<< HEAD
-=======
-
->>>>>>> 2751ca2 (1.index.js樂透邏輯)
 while(true){
     let r=Math.floor(Math.random()*49)+1;
     
@@ -25,29 +17,16 @@ while(true){
     number.push(r);
 }
 if (number.length==6){
+    // 數字填滿後再排序
+    number.sort(compare);
     break;
-}
-number.sort(compare);
-}
-<<<<<<< HEAD
-=======
-// 改變內文
->>>>>>> 2751ca2 (1.index.js樂透邏輯)
+}}
+
 testEl.innerHTML+=`<li>第${j+1}組，號碼:${number}</li>`
 lottroy.push(number);
 }
 testEl.innerHTML+=`</ol>`
 console.log(lottroy);
-
-
-
-
-
-
-
-
-
-
 
 
 function compare(a,b){
