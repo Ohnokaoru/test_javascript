13e689e21f5241e689aa76860ce7befadab635e2
Kaoru <kaoru@gmail.com>
2024-06-20 10:21:07 +0800

    修改lottroy小細節

--- lottroy_step.js
+++ lottroy_step.js
@@ -1,5 +1,11 @@
 
+<<<<<<< HEAD
 let testEl=document.querySelector("#test");
+=======
+// 抓取 <div id="test">
+let testEl=document.querySelector("#test");
+// 看內文
+>>>>>>> 2751ca2 (1.index.js樂透邏輯)
 console.log(testEl.textContent);
 testEl.innerHTML=`<ol>`
 
@@ -8,6 +14,10 @@ let lottroy=[];
 
 for(let j=0; j<count;j++){
 let number=[];
+<<<<<<< HEAD
+=======
+
+>>>>>>> 2751ca2 (1.index.js樂透邏輯)
 while(true){
     let r=Math.floor(Math.random()*49)+1;
     
@@ -19,6 +29,10 @@ if (number.length==6){
 }
 number.sort(compare);
 }
+<<<<<<< HEAD
+=======
+// 改變內文
+>>>>>>> 2751ca2 (1.index.js樂透邏輯)
 testEl.innerHTML+=`<li>第${j+1}組，號碼:${number}</li>`
 lottroy.push(number);
 }
